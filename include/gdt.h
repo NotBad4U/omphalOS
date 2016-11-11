941201c14ed4143632c28f12966d112e6c1827d8
NotBad4U <lightplay8@gmail.com>
2016-11-11 19:59:48 +0100

    Add the task state segment (TSS)
    The Task State Segment (TSS) is a special data structure for x86 processors which holds information about a task.
    The TSS is primarily suited for hardware multitasking, where each individual process has its own TSS.

--- include/gdt.h
+++ include/gdt.h
@@ -4,19 +4,21 @@
 #define GDTSIZE		0xFF	    /* nb max of descriptors in a table */
 #define GDTBASE		0x00000800	/* physical address of the gdt */
 
+#define SEGMENT_CODE_USER   0x30000     /* Segment for code user (arbitrary value)*/
+#define SEGMENT_STACK_USER  0x20000     /* Segment for stack user (arbitrary value)*/
 
 #include "types.h"
 #include "util.h"
 
 /* Reference the system where the gdt is initialized */
-struct gdtr {
+typedef struct {
     uint16 limite;
     uint32 base;
-} __attribute__ ((packed));
+} __attribute__ ((packed)) gdtr_t;
 
 /* Global descriptor table
 Describe segment */
-struct gdtdesc {
+typedef struct {
     uint16 lim0_15;
     uint16 base0_15;
     uint8 base16_23;
@@ -24,18 +26,38 @@ struct gdtdesc {
     uint8 lim16_19 : 4;
     uint8 other : 4;
     uint8 base24_31;
-} __attribute__ ((packed));
+} __attribute__ ((packed)) gdtdesc_t;
+
+typedef struct {
+    uint16 previousTask, previousTaskReserved;
+    uint32 esp0; // ESP0 gets the value the stack-pointer shall get at a system call
+    uint16 ss0, ss0Reserved; // SS0 gets the kernel datasegment descriptor
+    uint32 esp1;
+    uint16 ss1, ss1Reserved;
+    uint32 esp2;
+    uint16 ss2, ss2Reserved;
+    uint32 cr3;
+    uint32 eip;
+    uint32 eflags;
+    uint32 eax, ecx, edx, ebx;
+    uint32 esp;
+    uint32 ebp;
+    uint32 esi;
+    uint32 edi;
+    uint16 es, esReserved;
+    uint16 cs, csReserved;
+    uint16 ss, ssReserved;
+    uint16 ds, dsReserved;
+    uint16 fs, fsReserved;
+    uint16 gs, gsReserved;
+    uint16 ldtSegmentSelector, ldtSegmentSelectorReserved;
+    uint16 debugFlag, ioMapBaseAddres;
+
+} __attribute__((packed)) tss_t ;
+
 
 void initGdt(void);
 
-void init_gdt_desc(uint32 base, uint32 limite, uint8 acces, uint8 other, struct gdtdesc *desc);
-
-// #ifdef __GDT__
-// 	struct gdtdesc kgdt[GDTSIZE];	/* GDT */
-// 	struct gdtr kgdtr;		/* GDTR */
-// #else
-// 	extern struct gdtdesc kgdt[GDTSIZE];
-// 	extern struct gdtr kgdtr;
-// #endif
+void init_gdt_desc(uint32 base, uint32 limite, uint8 acces, uint8 other, gdtdesc_t *desc);
 
 #endif /* IDT_H */
--- src/gdt.c
+++ src/gdt.c
@@ -1,11 +1,14 @@
 #include "gdt.h"
 #include "screen.h"
 
-struct gdtdesc kgdt[GDTSIZE];	/* GDT with GDTSIZE segment */
-struct gdtr kgdtr;              /* GDTR */
+static void initDefaultTss();
+
+gdtdesc_t kgdt[GDTSIZE];	/* GDT with GDTSIZE segment */
+gdtr_t kgdtr;              /* GDTR */
+tss_t defaultTss;
 
 
-void init_gdt_desc(uint32 base, uint32 limite, uint8 acces, uint8 other, struct gdtdesc *desc)
+void init_gdt_desc(uint32 base, uint32 limite, uint8 acces, uint8 other, gdtdesc_t *desc)
 {
         desc->lim0_15 = (limite & 0xffff);
         desc->base0_15 = (base & 0xffff);
@@ -20,10 +23,19 @@ void init_gdt_desc(uint32 base, uint32 limite, uint8 acces, uint8 other, struct 
 
 void initGdt(void)
 {
+    initDefaultTss();
+    
     init_gdt_desc(0x0, 0x0, 0x0, 0x0, &kgdt[0]);
     init_gdt_desc(0x0, 0xFFFFF, 0x9B, 0x0D, &kgdt[1]);      /* code */
     init_gdt_desc(0x0, 0xFFFFF, 0x93, 0x0D, &kgdt[2]);      /* data */
     init_gdt_desc(0x0, 0x0, 0x97, 0x0D, &kgdt[3]);          /* stack */
+
+    /* segment for user code */
+    init_gdt_desc(SEGMENT_CODE_USER, 0x0, 0xFF, 0x0D, &kgdt[4]); /* ucode */
+    init_gdt_desc(SEGMENT_CODE_USER, 0x0, 0xF3, 0x0D, &kgdt[5]); /* udata */
+    init_gdt_desc(0x0, 0x20, 0xF7, 0x0D, &kgdt[6]); /* ustack */
+
+    init_gdt_desc((uint32) &defaultTss, 0x67, 0xE9, 0x00, &kgdt[7]); /* tss descriptor */
 
     kgdtr.limite = GDTSIZE * 8;
     kgdtr.base = GDTBASE;
@@ -41,4 +53,12 @@ void initGdt(void)
        next:               \n");
 
     return;
+}
+
+static void initDefaultTss()
+{
+    defaultTss.debugFlag = 0x00;
+    defaultTss.ioMapBaseAddres = 0x00;
+    defaultTss.esp0 = SEGMENT_STACK_USER;
+    defaultTss.ss0 = 0x18;
 }
